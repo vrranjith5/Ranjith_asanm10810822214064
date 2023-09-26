17253eda0148e8ddc183b760e51688f23b32927d
vrranjith5 <146019428+vrranjith5@users.noreply.github.com>
2023-09-26 22:39:47 +0530

    Update challenge 2.py

--- challenge 2.py
+++ challenge 2.py
@@ -1,9 +1,24 @@
+# 1.1 Implement a recursive function to calculate the factorial of a given number.
+"""
+1! = 1 Ã— 1
+2! = 2 Ã— 1! --->2 Ã— 1
+3! = 3 Ã— 2! --->3 Ã— 2 Ã— 1
+.
+.
+10! = 10 Ã— 9! --->10 Ã— 9 Ã— 8 Ã—... Ã—1
+
+Formula - n Ã— (n-1)!
+"""
+
+
 def fact_rec(n):
-  if n==0 or n==1:
+  if n == 0 or n == 1:
     return 1
   else:
-    return n*fact_rec(n-1)
+    return n * fact_rec(n - 1)
 
-number = 6
+
+number = int(input("Enter a value : "))
 res = fact_rec(number)
-print("the factorial of {} is {}". format(number,res))
+
+print("the factorial of {} is {}.".format(number, res))
